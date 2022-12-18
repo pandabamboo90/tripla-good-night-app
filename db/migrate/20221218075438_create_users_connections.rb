8ca49fc4508dc8236c96940e8990e65159817aee
Dang Nguyen <dang.nguyen@comestri.com>
2022-12-19 02:43:06 +0700

    feat: update migration for users_connections table

--- db/migrate/20221218075438_create_users_connections.rb
+++ db/migrate/20221218075438_create_users_connections.rb
@@ -1,8 +1,8 @@
-class CreateUserConnections < ActiveRecord::Migration[7.0]
+class CreateUsersConnections < ActiveRecord::Migration[7.0]
   def change
     create_table :users_connections do |t|
-      t.references :source_user, null: false, foreign_key: true, index: true
-      t.references :target_user, null: false, foreign_key: true, index: true
+      t.references :source_user, foreign_key: { to_table: :users }, null: false, index: true
+      t.references :target_user, foreign_key: { to_table: :users }, null: false, index: true
       t.integer :status
 
       t.timestamps
