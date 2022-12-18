3c640bd5ddf3cf3d8b1f2f80592e793db5b411d3
Dang Nguyen <dang.nguyen@comestri.com>
2022-12-19 02:42:45 +0700

    feat: add method to seed sleep records data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,22 @@
+def seed_1w_sleep_record(user:)
+  for idx in -6..0
+    today = Time.zone.now
+    started_at = today + idx.day
+    ended_at = started_at + (Faker::Number.between(from: 4, to: 10).round(2)).hours
+
+    sleep_record_params = {
+      started_at: started_at,
+      ended_at: ended_at
+    }.with_indifferent_access
+    user.add_sleep_record!(sleep_record_params: sleep_record_params)
+  end
+end
+
+
 ActiveRecord::Base.transaction do
   5.times do |idx|
-    User.create!(name: Faker::Name.name)
+    user = User.create!(name: Faker::Name.name)
+
+    seed_1w_sleep_record(user: user)
   end
 end
