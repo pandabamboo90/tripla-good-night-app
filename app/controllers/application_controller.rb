a114b0ebac34a19de6a7378565fdbe9417248e1b
Dang Nguyen <dang.nguyen@comestri.com>
2022-12-21 00:39:39 +0700

    feat: add error handlers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,46 @@ class ApplicationController < ActionController::API
   before_action :set_current_user
   def set_current_user
     @current_user = User.find(request.headers['current-user-id'])
+    render_errors(status: 401, title: 'Unauthenticated', detail: 'Unauthenticated') if @current_user.blank?
   end
 
   def index
     render json: { message: 'API online' }
+  end
+
+  # ---------------------------------
+  # Error handlers
+  # ---------------------------------
+
+  rescue_from StandardError do |exception|
+    render_errors(title: 'Internal server error', detail: exception.message, status: 500)
+  end
+
+  rescue_from ArgumentError,
+              ActionController::ParameterMissing do |exception|
+    render_errors(title: 'Bad Request', detail: exception.message, status: 400)
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    detail = exception.record.errors.map { |error|
+      {field: error.attribute, detail: error.full_message}
+    }
+
+    render_errors(title: 'Record invalid', detail: detail, status: 422)
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render_errors(title: 'Not found', detail: exception.message, status: 404)
+  end
+
+  def render_errors(status:, title:, detail:)
+    render json: {
+      errors: [
+        {
+          title: title,
+          detail: detail
+        }
+      ]
+    }, status: status
   end
 end
